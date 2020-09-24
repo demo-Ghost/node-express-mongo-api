5dc64e8cc127e206580a62e977b6067f0aac363a
demo-Ghost <ant.nikolaidis@gmail.com>
2020-09-24 23:30:46 +0300

    Add some comments for readability. Add test route.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 
+// config vars
 const port = process.env.port;
 
+// initialize
 const app = express();
 
+// middleware
 app.use(cors());
+
+// routes
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
 
 app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`),
