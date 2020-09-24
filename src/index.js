e3ccb7792ddcabe708c7ab637abeb44d2a3f8284
demo-Ghost <ant.nikolaidis@gmail.com>
2020-09-24 23:40:22 +0300

    More routes for simple testing.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import express from 'express';
 
 // config vars
-const port = process.env.port;
+const port = process.env.PORT;
 
 // initialize
 const app = express();
@@ -13,9 +13,25 @@ const app = express();
 app.use(cors());
 
 // routes
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
+app.get('/users', (req, res) => {
+  return res.send('GET HTTP method on user resource');
+});
+ 
+app.post('/users', (req, res) => {
+  return res.send('POST HTTP method on user resource');
+});
+ 
+app.put('/users/:userId', (req, res) => {
+  return res.send(
+    `PUT HTTP method on user/${req.params.userId} resource`,
+  );
+});
+ 
+app.delete('/users/:userId', (req, res) => {
+  return res.send(
+    `DELETE HTTP method on user/${req.params.userId} resource`,
+  );
+});
 
 app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`),
