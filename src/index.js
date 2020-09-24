7b70b82adffa8951215112c17b5bbf2ee82dae57
demo-Ghost <ant.nikolaidis@gmail.com>
2020-09-24 23:28:39 +0300

    Install and initialize express + cors. Add app-level cors middleware. Pass port from env vars.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import 'dotenv/config';
 
-console.log(" I\'m running here ...");
+import cors from 'cors';
+import express from 'express';
+
+const port = process.env.port;
+
+const app = express();
+
+app.use(cors());
+
+app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`),
+);
