606f871f724668b64758bd58b31392697a945874
demo-Ghost <ant.nikolaidis@gmail.com>
2020-09-25 10:25:10 +0300

    Make routes modular with express Router. Separate from app logic.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import express from 'express';
 
 import models from './models';
+import routes from './routes';
 
 // config vars
 const port = process.env.PORT || 8080;
@@ -25,25 +26,7 @@ app.use((req, res, next) => {
 });
 
 // routes
-app.get('/users', (req, res) => {
-  return res.send('GET HTTP method on user resource');
-});
- 
-app.post('/users', (req, res) => {
-  return res.send('POST HTTP method on user resource');
-});
- 
-app.put('/users/:userId', (req, res) => {
-  return res.send(
-    `PUT HTTP method on user/${req.params.userId} resource`,
-  );
-});
- 
-app.delete('/users/:userId', (req, res) => {
-  return res.send(
-    `DELETE HTTP method on user/${req.params.userId} resource`,
-  );
-});
+app.use('/users', routes.user);
 
 app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`),
